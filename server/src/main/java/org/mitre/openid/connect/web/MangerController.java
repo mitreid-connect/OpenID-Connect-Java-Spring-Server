c0c21fc31fc5f7f221d1f0b417dbe535f3f80428
Michael Jett <mjett@mitre.org>
2012-02-20 16:16:19 -0500

    MangerController initial check-in

---
+++ server/src/main/java/org/mitre/openid/connect/web/MangerController.java
@@ -0,0 +1,17 @@
+package org.mitre.openid.connect.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Michael Jett <mjett@mitre.org>
+ */
+
+@Controller
+public class MangerController {
+
+    @RequestMapping({"/", "/home", "/index"})
+    public String showHomePage() {
+        return "home";
+    }
+}
