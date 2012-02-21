b7c99e30c5a8b7ecf23a910bf995ee68ae34773e
Michael Jett <mjett@mitre.org>
2012-02-21 13:08:23 -0500

    Fixing typo

--- server/src/main/java/org/mitre/openid/connect/web/ManagerController.java
+++ server/src/main/java/org/mitre/openid/connect/web/ManagerController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 
 @Controller
-public class MangerController {
+public class ManagerController {
 
     @RequestMapping({"/", "/home", "/index"})
     public String showHomePage() {
