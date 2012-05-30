2a831a4e016b37c0cb138c0525c195ba673f79b8
nemonik <github.com@nemonik.com>
2012-05-30 15:14:15 -0400

    see issue #19

--- openid-connect-server/src/main/java/org/mitre/openid/connect/exception/ExpiredTokenException.java
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/exception/ExpiredTokenException.java
@@ -15,11 +15,20 @@
  ******************************************************************************/
 package org.mitre.openid.connect.exception;
 
+/**
+ * @author aanganes, nemonik
+ *
+ */
 public class ExpiredTokenException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	public ExpiredTokenException() {
+		super();
+	}
+
+	public ExpiredTokenException(String message) {
+		super(message);
+	}
+	
 }
