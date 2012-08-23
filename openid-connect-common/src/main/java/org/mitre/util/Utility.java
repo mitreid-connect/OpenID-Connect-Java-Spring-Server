ba24ca1f60bb2f3ccf31dfd71523f3f3ad7059c3
Justin Richer <jricher@mitre.org>
2012-08-23 13:49:29 -0400

    removed unused utility method

--- openid-connect-common/src/main/java/org/mitre/util/Utility.java
+++ openid-connect-common/src/main/java/org/mitre/util/Utility.java
@@ -23,25 +23,6 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class Utility {
 
-	/**
-	 * Returns the base URL from a HttpServletRequest
-	 * 
-	 * @param request
-	 * @return
-	 */
-	public static String findBaseUrl(HttpServletRequest request) {
-		String issuer = String.format("%s://%s%s", request.getScheme(),
-				request.getServerName(), request.getContextPath());
-
-		if ((request.getScheme().equals("http") && request.getServerPort() != 80)
-				|| (request.getScheme().equals("https") && request
-						.getServerPort() != 443)) {
-			// nonstandard port, need to include it
-			issuer = String.format("%s://%s:%d%s", request.getScheme(),
-					request.getServerName(), request.getServerPort(),
-					request.getContextPath());
-		}
-		return issuer;
-	}
+	// currently empty
 
 }
