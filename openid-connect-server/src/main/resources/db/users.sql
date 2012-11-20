4fdb0816ebddaef819c87f70fd83e1d5d1a996dc
Stephen Moore <srmoore@mitre.org>
2012-11-20 16:37:30 -0500

    Moved DB to use in memory HSQLDB. Made authentication-provider use a jdbc-user-service in that in-memory hsqldb.

---
+++ openid-connect-server/src/main/resources/db/users.sql
@@ -0,0 +1,23 @@
+INSERT INTO users(username, password, enabled) values ('jricher','password',true);
+INSERT INTO authorities(username,authority) values ('jricher','ROLE_USER');
+INSERT INTO authorities(username,authority) values ('jricher','ROLE_ADMIN');
+
+INSERT INTO users(username, password, enabled) values ('mfranklin','password',true);
+INSERT INTO authorities(username,authority) values ('mfranklin','ROLE_USER');
+INSERT INTO authorities(username,authority) values ('mfranklin','ROLE_ADMIN');
+
+INSERT INTO users(username, password, enabled) values ('dcuomo','password',true);
+INSERT INTO authorities(username,authority) values ('dcuomo','ROLE_USER');
+INSERT INTO authorities(username,authority) values ('dcuomo','ROLE_ADMIN');
+
+INSERT INTO users(username, password, enabled) values ('aanganes','password',true);
+INSERT INTO authorities(username,authority) values ('aanganes','ROLE_USER');
+INSERT INTO authorities(username,authority) values ('aanganes','ROLE_ADMIN');
+
+INSERT INTO users(username, password, enabled) values ('mjwalsh','password',true);
+INSERT INTO authorities(username,authority) values ('mjwalsh','ROLE_USER');
+INSERT INTO authorities(username,authority) values ('mjwalsh','ROLE_ADMIN');
+
+INSERT INTO users(username, password, enabled) values ('srmoore','password',true);
+INSERT INTO authorities(username,authority) values ('srmoore','ROLE_USER');
+INSERT INTO authorities(username,authority) values ('srmoore','ROLE_ADMIN');
