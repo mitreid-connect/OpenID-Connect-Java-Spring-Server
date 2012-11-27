250432ce7f93c01f793619c1c2d3afdc44a9a701
Stephen Moore <srmoore@mitre.org>
2012-11-27 12:57:10 -0500

    Added information into the user_info table

--- openid-connect-server/src/main/resources/db/users.sql
+++ openid-connect-server/src/main/resources/db/users.sql
@@ -6,19 +6,24 @@ INSERT INTO user_info(user_id, preferred_username, name, email_verified) values 
 INSERT INTO users(username, password, enabled) values ('mfranklin','password',true);
 INSERT INTO authorities(username,authority) values ('mfranklin','ROLE_USER');
 INSERT INTO authorities(username,authority) values ('mfranklin','ROLE_ADMIN');
+INSERT INTO user_info(user_id, preferred_username, name, email_verified) values ('mfranklin','mfranklin','mfranklin', 'FALSE');
 
 INSERT INTO users(username, password, enabled) values ('dcuomo','password',true);
 INSERT INTO authorities(username,authority) values ('dcuomo','ROLE_USER');
 INSERT INTO authorities(username,authority) values ('dcuomo','ROLE_ADMIN');
+INSERT INTO user_info(user_id, preferred_username, name, email_verified) values ('dcuomo','dcuomo','dcuomo', 'FALSE');
 
 INSERT INTO users(username, password, enabled) values ('aanganes','password',true);
 INSERT INTO authorities(username,authority) values ('aanganes','ROLE_USER');
 INSERT INTO authorities(username,authority) values ('aanganes','ROLE_ADMIN');
+INSERT INTO user_info(user_id, preferred_username, name, email_verified) values ('aanganes','aanganes','aanganes', 'FALSE');
 
 INSERT INTO users(username, password, enabled) values ('mjwalsh','password',true);
 INSERT INTO authorities(username,authority) values ('mjwalsh','ROLE_USER');
 INSERT INTO authorities(username,authority) values ('mjwalsh','ROLE_ADMIN');
+INSERT INTO user_info(user_id, preferred_username, name, email_verified) values ('mjwalsh','mjwalsh','mjwalsh', 'FALSE');
 
 INSERT INTO users(username, password, enabled) values ('srmoore','password',true);
 INSERT INTO authorities(username,authority) values ('srmoore','ROLE_USER');
 INSERT INTO authorities(username,authority) values ('srmoore','ROLE_ADMIN');
+INSERT INTO user_info(user_id, preferred_username, name, email_verified) values ('srmoore','srmoore','srmoore', 'FALSE');
