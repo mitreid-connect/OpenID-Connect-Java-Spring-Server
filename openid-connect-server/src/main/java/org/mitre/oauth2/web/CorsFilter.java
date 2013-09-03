9020261d2c2c8c1c8526ec08c45bce6226c89306
Justin Richer <jricher@mitre.org>
2013-09-03 15:10:17 -0400

    added CORS filter

---
+++ openid-connect-server/src/main/java/org/mitre/oauth2/web/CorsFilter.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.mitre.oauth2.web;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+/**
+ * @author jricher
+ *
+ */
+@Component("corsFilter")
+public class CorsFilter extends GenericFilterBean {
+
+	/* (non-Javadoc)
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest)req;
+		HttpServletResponse response = (HttpServletResponse)resp;
+		
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
+			// CORS "pre-flight" request
+			response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+			response.addHeader("Access-Control-Allow-Headers", "X-Requested-With,Origin,Content-Type, Accept, Authorization");
+		}
+		filterChain.doFilter(request, response);
+	}
+
+}
