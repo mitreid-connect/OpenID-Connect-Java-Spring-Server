f13954148527e63f45846d48d14f0eaac549b2a6
Justin Richer <jricher@mitre.org>
2013-09-10 13:48:37 -0400

    added randomized subs to demo users

--- openid-connect-server/src/main/resources/db/users.sql
+++ openid-connect-server/src/main/resources/db/users.sql
@@ -22,8 +22,8 @@ INSERT INTO authorities_TEMP (username, authority) VALUES
     
 -- By default, the username column here has to match the username column in the users table, above
 INSERT INTO user_info_TEMP (sub, preferred_username, name, email, email_verified) VALUES
-  ('admin','admin','Demo Admin','admin@example.com',false),
-  ('user','user','Demo User','user@example.com',false);
+  ('90342.ASDFJWFA','admin','Demo Admin','admin@example.com', true),
+  ('01921.FLANRJQW','user','Demo User','user@example.com', true);
 
  
 --
