1ef767462e669eb5ddf17d45a6e2cf0386bc2886
Justin Richer <jricher@mit.edu>
2014-03-13 20:05:20 -0400

    local patch to retina.js to make it play nicer in firefox

--- openid-connect-server-webapp/src/main/webapp/resources/js/lib/retina.js
+++ openid-connect-server-webapp/src/main/webapp/resources/js/lib/retina.js
@@ -80,7 +80,7 @@
       return callback(true);
     } else {
       http = new XMLHttpRequest;
-      http.open('HEAD', this.at_2x_path);
+      http.open('GET', this.at_2x_path);
       http.onreadystatechange = function() {
         if (http.readyState != 4) {
           return callback(false);
