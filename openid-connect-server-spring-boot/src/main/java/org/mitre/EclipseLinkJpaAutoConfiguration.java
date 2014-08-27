1e76937000dca40f57af1c9b6bb32cd425870fab
Wolter Eldering <wolter.eldering@enovation.nl>
2014-08-27 15:54:26 +0200

    IAM-9 Initial checking

---
+++ openid-connect-server-spring-boot/src/main/java/org/mitre/EclipseLinkJpaAutoConfiguration.java
@@ -0,0 +1,81 @@
+package org.mitre;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaBaseConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.AbstractJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.EclipseLinkJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.ClassUtils;
+
+import org.mitre.EclipseLinkJpaAutoConfiguration.EclipseLinkEntityManagerCondition;
+
+/**
+ * {@link EnableAutoConfiguration Auto-configuration} for Hibernate JPA.
+ *
+ * @author Phillip Webb
+ */
+@Configuration
+@ConditionalOnClass({ LocalContainerEntityManagerFactoryBean.class,
+		EnableTransactionManagement.class, EntityManager.class })
+@Conditional(EclipseLinkEntityManagerCondition.class)
+@AutoConfigureAfter(DataSourceAutoConfiguration.class)
+public class EclipseLinkJpaAutoConfiguration extends JpaBaseConfiguration {
+
+	@Autowired
+	private JpaProperties properties;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private ConfigurableApplicationContext applicationContext;
+
+	@Override
+	protected AbstractJpaVendorAdapter createJpaVendorAdapter() {
+		return new EclipseLinkJpaVendorAdapter();
+	}
+
+	@Override
+	protected Map<String, String> getVendorProperties() {
+		Map<String, String> vendorProperties = new LinkedHashMap<String, String>();
+		vendorProperties.putAll(this.properties.getHibernateProperties(this.dataSource));
+		return vendorProperties;
+	}
+
+	static class EclipseLinkEntityManagerCondition extends SpringBootCondition {
+
+		private static String[] CLASS_NAMES = {
+				"org.eclipse.persistence.jpa.JpaEntityManager"};
+
+		@Override
+		public ConditionOutcome getMatchOutcome(ConditionContext context,
+				AnnotatedTypeMetadata metadata) {
+			for (String className : CLASS_NAMES) {
+				if (ClassUtils.isPresent(className, context.getClassLoader())) {
+					return ConditionOutcome.match("found HibernateEntityManager class");
+				}
+			}
+			return ConditionOutcome.noMatch("did not find HibernateEntityManager class");
+		}
+	}
+
+}
---
+++ openid-connect-server-spring-boot/src/main/resources/templates/login.html
@@ -0,0 +1,49 @@
+<%@ taglib prefix="authz" uri="http://www.springframework.org/security/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="o" tagdir="/WEB-INF/tags"%>
+<o:header title="Log In" />
+<script type="text/javascript">
+<!--
+
+$(document).ready(function() {
+	$('#j_username').focus();
+});
+
+//-->
+</script>
+<o:topbar />
+<div class="container-fluid main">
+
+	<h1>Login with Username and Password</h1>
+
+	<c:if test="${ param.error != null }">
+		<div class="alert alert-error">The system was unable to log you
+			in. Please try again.</div>
+	</c:if>
+
+
+	<div class="row-fluid">
+		<div class="span4 offset1 well">
+			<form action="<%=request.getContextPath()%>/j_spring_security_check"
+				method="POST">
+				<div>
+					<div class="input-prepend input-block-level">
+						<span class="add-on"><i class="icon-user"></i></span>
+						<input type="text" placeholder="Username" autocorrect="off" autocapitalize="off" autocomplete="off" spellcheck="false" value="" id="j_username" name="j_username">
+					</div>
+				</div>
+				<div>
+					<div class="input-prepend input-block-level">
+						<span class="add-on"><i class="icon-lock"></i></span>
+						<input type="password" placeholder="Password" autocorrect="off" autocapitalize="off" autocomplete="off" spellcheck="false" id="j_password" name="j_password">
+					</div>
+				</div>
+				<div>
+					<input type="submit" class="btn" value="Login" name="submit">
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+<o:footer/>
