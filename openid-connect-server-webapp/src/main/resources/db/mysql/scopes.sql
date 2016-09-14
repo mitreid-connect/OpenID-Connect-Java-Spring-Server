1b7612a26d31a0119c4c4d26f54e87297aa45a93
Neths <vacher.michel@gmail.com>
2016-09-14 16:18:26 +0200

    add db init script for mysql add data-context example with mysql db initializer

---
+++ openid-connect-server-webapp/src/main/resources/db/mysql/scopes.sql
@@ -0,0 +1,31 @@
+--
+-- Turn off autocommit and start a transaction so that we can use the temp tables
+--
+
+SET AUTOCOMMIT = 0;
+
+START TRANSACTION;
+
+--
+-- Insert scope information into the temporary tables.
+-- 
+
+INSERT INTO system_scope_TEMP (scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
+  ('openid', 'log in using your identity', 'user', false, true, false, null),
+  ('profile', 'basic profile information', 'list-alt', false, true, false, null),
+  ('email', 'email address', 'envelope', false, true, false, null),
+  ('address', 'physical address', 'home', false, true, false, null),
+  ('phone', 'telephone number', 'bell', false, true, false, null),
+  ('offline_access', 'offline access', 'time', false, false, false, null);
+  
+--
+-- Merge the temporary scopes safely into the database. This is a two-step process to keep scopes from being created on every startup with a persistent store.
+--
+
+INSERT INTO system_scope (scope, description, icon, restricted, default_scope, structured, structured_param_description) 
+  SELECT scope, description, icon, restricted, default_scope, structured, structured_param_description FROM system_scope_TEMP
+  ON DUPLICATE KEY UPDATE system_scope.scope = system_scope.scope;
+
+COMMIT;
+
+SET AUTOCOMMIT = 1;
