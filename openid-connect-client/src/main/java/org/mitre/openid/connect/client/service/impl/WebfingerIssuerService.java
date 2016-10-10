d0056ae88212ed34c5b52ddebd1130c5e96fadf8
Justin Richer <jricher@mit.edu>
2016-10-10 15:32:32 -0400

    added target link URI capability to webfinger issuer service

--- openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/impl/WebfingerIssuerService.java
+++ openid-connect-client/src/main/java/org/mitre/openid/connect/client/service/impl/WebfingerIssuerService.java
@@ -114,8 +114,8 @@ public class WebfingerIssuerService implements IssuerService {
 				if (blacklist.contains(lr.issuer)) {
 					throw new AuthenticationServiceException("Issuer was in blacklist: " + lr.issuer);
 				}
-
-				return new IssuerServiceResponse(lr.issuer, lr.loginHint, null);
+				
+				return new IssuerServiceResponse(lr.issuer, lr.loginHint, request.getParameter("target_link_uri"));
 			} catch (UncheckedExecutionException | ExecutionException e) {
 				logger.warn("Issue fetching issuer for user input: " + identifier + ": " + e.getMessage());
 				return null;
