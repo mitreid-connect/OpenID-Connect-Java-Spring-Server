bf49cd193d31f91c4ca2439d889856f187cc98c6
Justin Richer <jricher@mit.edu>
2017-04-06 14:07:39 -0400

    removed incompatible constraints on mysql files

--- openid-connect-server-webapp/src/main/resources/db/mysql/mysql_database_tables.sql
+++ openid-connect-server-webapp/src/main/resources/db/mysql/mysql_database_tables.sql
@@ -10,8 +10,7 @@ CREATE TABLE IF NOT EXISTS access_token (
 	refresh_token_id BIGINT,
 	client_id BIGINT,
 	auth_holder_id BIGINT,
-	approved_site_id BIGINT,
-	UNIQUE(token_value)
+	approved_site_id BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS access_token_permissions (
@@ -383,17 +382,17 @@ CREATE TABLE IF NOT EXISTS device_code_request_parameter (
 	val VARCHAR(2048)
 );
 
-CREATE INDEX IF NOT EXISTS at_tv_idx ON access_token(token_value(767));
-CREATE INDEX IF NOT EXISTS ts_oi_idx ON token_scope(owner_id);
-CREATE INDEX IF NOT EXISTS at_exp_idx ON access_token(expiration);
-CREATE INDEX IF NOT EXISTS rf_ahi_idx ON refresh_token(auth_holder_id);
-CREATE INDEX IF NOT EXISTS cd_ci_idx ON client_details(client_id);
-CREATE INDEX IF NOT EXISTS at_ahi_idx ON access_token(auth_holder_id);
-CREATE INDEX IF NOT EXISTS aha_oi_idx ON authentication_holder_authority(owner_id);
-CREATE INDEX IF NOT EXISTS ahe_oi_idx ON authentication_holder_extension(owner_id);
-CREATE INDEX IF NOT EXISTS ahrp_oi_idx ON authentication_holder_request_parameter(owner_id);
-CREATE INDEX IF NOT EXISTS ahri_oi_idx ON authentication_holder_resource_id(owner_id);
-CREATE INDEX IF NOT EXISTS ahrt_oi_idx ON authentication_holder_response_type(owner_id);
-CREATE INDEX IF NOT EXISTS ahs_oi_idx ON authentication_holder_scope(owner_id);
-CREATE INDEX IF NOT EXISTS ac_ahi_idx ON authorization_code(auth_holder_id);
-CREATE INDEX IF NOT EXISTS suaa_oi_idx ON saved_user_auth_authority(owner_id);
+CREATE INDEX at_tv_idx ON access_token(token_value(767));
+CREATE INDEX ts_oi_idx ON token_scope(owner_id);
+CREATE INDEX at_exp_idx ON access_token(expiration);
+CREATE INDEX rf_ahi_idx ON refresh_token(auth_holder_id);
+CREATE INDEX cd_ci_idx ON client_details(client_id);
+CREATE INDEX at_ahi_idx ON access_token(auth_holder_id);
+CREATE INDEX aha_oi_idx ON authentication_holder_authority(owner_id);
+CREATE INDEX ahe_oi_idx ON authentication_holder_extension(owner_id);
+CREATE INDEX ahrp_oi_idx ON authentication_holder_request_parameter(owner_id);
+CREATE INDEX ahri_oi_idx ON authentication_holder_resource_id(owner_id);
+CREATE INDEX ahrt_oi_idx ON authentication_holder_response_type(owner_id);
+CREATE INDEX ahs_oi_idx ON authentication_holder_scope(owner_id);
+CREATE INDEX ac_ahi_idx ON authorization_code(auth_holder_id);
+CREATE INDEX suaa_oi_idx ON saved_user_auth_authority(owner_id);
