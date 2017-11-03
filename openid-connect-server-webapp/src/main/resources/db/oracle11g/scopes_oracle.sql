f1d8c9b37e32aeb9a901577b69772d30e77b4259
Angelo Kastroulis <angelo@kastroulis.com>
2017-11-03 14:14:05 -0400

    Added Oracle11g Support

--- openid-connect-server-webapp/src/main/resources/db/oracle11g/scopes_oracle.sql
+++ openid-connect-server-webapp/src/main/resources/db/oracle11g/scopes_oracle.sql
@@ -19,14 +19,3 @@ INSERT INTO system_scope (id, scope, description, icon, restricted, default_scop
 INSERT INTO system_scope (id, scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
   (system_scope_seq.nextval, 'online_access', 'offline access', 'time', 0, 0, 0, null);
 
-INSERT INTO system_scope (id, scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  (system_scope_seq.nextval, 'launch/patient', 'Need patient context at launch time (FHIR Patient resource)', 'user', 0, 0, 0, null);
-
-INSERT INTO system_scope (id, scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  (system_scope_seq.nextval, 'launch/encounter', 'Need encounter context at launch time (FHIR Encounter resource)', 'user', 0, 0, 0, null);
-
-INSERT INTO system_scope (id, scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  (system_scope_seq.nextval, 'launch/location', 'Need location context at launch time (FHIR Location resource)', 'user', 0, 0, 0, null);
-
-INSERT INTO system_scope (id, scope, description, icon, restricted, default_scope, structured, structured_param_description) VALUES
-  (system_scope_seq.nextval, 'launch', 'Permission to obtain launch context when app is launched from an EHR', 'user', 0, 0, 0, null);
