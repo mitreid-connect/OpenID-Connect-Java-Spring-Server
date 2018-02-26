3c2549faf2a8119cd20512a4645cbe51fc4a1ded
enricovianello <maverick17586@gmail.com>
2018-02-26 16:45:27 +0100

    Cosmetic fix on DefaultIdTokenClaimsEnhancer

--- openid-connect-server/src/main/java/org/mitre/openid/connect/service/impl/DefaultIdTokenClaimsEnhancer.java
+++ openid-connect-server/src/main/java/org/mitre/openid/connect/service/impl/DefaultIdTokenClaimsEnhancer.java
@@ -3,8 +3,6 @@ package org.mitre.openid.connect.service.impl;
 import java.util.Date;
 import org.mitre.oauth2.model.OAuth2AccessTokenEntity;
 import org.mitre.openid.connect.service.IDTokenClaimsEnhancer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.stereotype.Service;
 import com.nimbusds.jwt.JWTClaimsSet;
@@ -12,16 +10,10 @@ import com.nimbusds.jwt.JWTClaimsSet;
 @Service("defaultIdTokenClaimsEnhancer")
 public class DefaultIdTokenClaimsEnhancer implements IDTokenClaimsEnhancer {
 
-  /**
-   * Logger for this class
-   */
-  private static final Logger logger = LoggerFactory.getLogger(DefaultOIDCTokenService.class);
-
   @Override
   public void enhanceIdTokenClaims(JWTClaimsSet.Builder claimsBuilder, OAuth2Request request, Date issueTime,
       String sub, OAuth2AccessTokenEntity accessToken) {
 
-    logger.debug("Enhancing Id-Token claims: no claims added.");
   }
 
 }
